4d389e445baa3cc1109ebcf16bff79d621ac225d
dubuchick <76254899+dubuchick@users.noreply.github.com>
2021-12-07 21:35:45 +0700

    aaa

---
+++ database/migrations/2021_12_07_143107_create_transaction_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->date('transactiondate');
+            $table->unsignedBigInteger('transactionid');
+            $table->foreign('transactionid')->references('id')->on('transaction');
+            $table->unsignedBigInteger('userid');
+            $table->foreign('userid')->references('id')->on('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction');
+    }
+}
