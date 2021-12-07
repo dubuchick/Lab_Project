c73f76e56a81571b48bd99212427b079d58cbb47
jonathanbryanw <jonathanbryanw@gmail.com>
2021-12-07 21:03:11 +0700

    Updated Layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,42 +8,39 @@
 </head>
 <body>
 
-<div class="container-fluid p-1 bg-primary text-white ">
+<div class="container-fluid p-1 bg-primary text-white">
     <div class="row mx-auto my-auto">
         <p class="text-start col-md-6">Book Store</p>
         <p class="text-end col-md-5">Register</p>
         <p class="text-end col-md-1">Login</p>
     </div>
-  
+
 </div>
 <div class="container">
-    
+
 </div>
 <div class="container mt-5">
     <div class="row ">
 
-        <div class="col-10 p-3">
+        <div class="col-12 p-3">
             @yield('content')
         </div>
 
         <div class = "p-3 col-2">
-            
+
         </div>
 
     </div>
-    
+
     <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 </div>
-<div class="footer">
-    <div class="container-fluid p-3 mt-5 bg-primary text-white">
-        <div class="text-start">
-            <small>Current date and time: {{ date("D") }}, {{ date("d M") }}, {{ date("Y") }} {{ date("H:i:a") }}</small>
-        </div>
-        <div class="text-center">
-            <small>&copy {{ date("Y") }} Book Store</small>
-        </div>
+<div class="container-fluid p-3 mt-5 bg-primary text-white">
+    <div class="text-start">
+        <small>Current date and time: {{ date("D") }}, {{ date("d M") }}, {{ date("Y") }} {{ date("H:i:a") }}</small>
+    </div>
+    <div class="text-center">
+        <small>&copy {{ date("Y") }} Book Store</small>
     </div>
 </div>
-
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layout');
-});
-
-Route::get('/manage', function () {
     return view('manage');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/register', function () {
-    return view('register');
 });
