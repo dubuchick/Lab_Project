a4fdf4c6fc58c374bd7feea2dfd623da369afaf5
jonathanbryanw <jonathanbryanw@gmail.com>
2021-12-14 22:40:09 +0700

    Books Detail Page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('/manage', function () {
+    return view('manage');
+});
+
+Route::get('/detail', function () {
+    return view('detail');
+});
